993b7094596474e5d80000faef20ff7c9a278abd
gift kimani <gift.kimani@students.moringaschool.com>
2023-06-28 22:35:06 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",function(){
+   const textChanger = document.getElementById("text");
+   textChanger.textContent = "This is really cool!";
+   console.log(textChanger);
+});
